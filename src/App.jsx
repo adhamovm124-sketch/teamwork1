e6e915cdf6d2f820f77f4d6b4ab317d513d398e1
jahongir vohidov <sobirjonovazizbek22@gmail.com>
2026-01-22 14:03:37 +0500

    ssd

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>salmontillabola</h1>
+      {/* <h1>salmontillabola</h1> */}
       <Section2/>
     </>
   )
--- src/component/Section2.jsx
+++ src/component/Section2.jsx
@@ -1,15 +1,325 @@
+import like from "../assets/like.png";
+import car1 from "../assets/car-img.png"
+import L90 from "../assets/90litr.png"
+import manul from "../assets/manul.png"
+import people2 from "../assets/2people.png"
+import car2 from "../assets/car2.png"
+import car3 from "../assets/Car3.png"
+import car5 from "../assets/Car5.png"
+import car6 from "../assets/car6.png"
+import car7 from "../assets/Car7.png"
+import car8 from "../assets/Car8.png"
+import car9 from "../assets/Car9.png"
+import car10 from "../assets/Car10.png"
+import car12 from "../assets/Car12.png"
 import "./Section2.css";
 function Section2() {
+    
     return (
+        <>
         <section className="section2">
             <div className="section2_div">
-                <h1>Azizbek sobirjonov</h1>
-                <h2>salmol</h2>
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>Koenigsegg</h3>
+                        <img src={like}/></div>
+                
+                    <p>Sport</p>
+                    <img src={car1}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$99.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>Nissan GT - R</h3>
+                        <img src={like}/></div>
+                
+                    <p>Sport</p>
+                    <img src={car2}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>Rolls - Royce</h3>
+                        <img src={like}/></div>
+                
+                    <p>Sedan</p>
+                    <img src={car3}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$96.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>Nissan GT - R</h3>
+                        <img src={like}/></div>
+                
+                    <p>Sport</p>
+                    <img src={car2}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+                
+                
+                
+                
             </div>
 
 
 
+
         </section>
+
+<p className="aloxida_p">Recomendation Car</p>
+<br />
+
+        <section className="section2">
+            <div className="section2_div">
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>All New Rush</h3>
+                        <img src={like}/></div>
+                
+                    <p>Suv</p>
+                    <img src={car5}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$72.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>CR  - V</h3>
+                        <img src={like}/></div>
+                
+                    <p>Suv</p>
+                    <img src={car6}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>All New Terios</h3>
+                        <img src={like}/></div>
+                
+                    <p>Suv</p>
+                    <img src={car7}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$74.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>CR  - V</h3>
+                        <img src={like}/></div>
+                
+                    <p>Suv</p>
+                    <img src={car8}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+                
+                
+                
+                
+            </div>
+
+
+
+
+        </section>
+
+<br />
+<section className="section2">
+            <div className="section2_div">
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>MG ZX Exclusice</h3>
+                        <img src={like}/></div>
+                
+                    <p>Hatchback</p>
+                    <img src={car9}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$99.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>New MG ZS</h3>
+                        <img src={like}/></div>
+                
+                    <p>Suv</p>
+                    <img src={car10}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>MG ZX Excite</h3>
+                        <img src={like}/></div>
+                
+                    <p>Hatchback</p>
+                    <img src={car9}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$74.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+                <div className="car_box1">
+                    <div className="car_box-div1">
+                        <h3>New MG ZS</h3>
+                        <img src={like}/></div>
+                
+                    <p>Sport</p>
+                    <img src={car12}/>
+                    <div className="car_box-div1_1">
+                        <img src={L90}/>
+                        <img src={manul}/>
+                        <img src={people2}/>
+                    </div>
+                    <div className="car_box-button">
+                        <h2>$80.00/day</h2>
+                        <button>Rent Now</button>
+                    </div>
+                </div>
+                
+                
+                
+                
+            </div>
+
+
+                <button className="car_button">Show more car</button>
+
+        </section>
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
+
+        </>
     );
 }
 export default Section2;
